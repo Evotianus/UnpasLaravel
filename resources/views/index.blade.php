7400be4f41f90b6eca461cdc87ffcd919fd376d0
VanillaMiko <vanillamiko0110@gmail.com>
2021-07-05 19:16:48 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layout/main')
+
+@section('title', 'Web Laravel')
+
+@section('container')
+<div class="container">
+    <div class="row">
+        <h1 class="mt-3">Hello, world!</h1>
+    </div>
+</div>
+@endsection
